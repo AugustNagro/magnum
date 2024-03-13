00e3f1ca5aa4bf06ebbb71b3647c9f401e58e88d
augustnagro <augustnagro@gmail.com>
2024-03-12 22:50:47 -0700

    iterator streaming for Query and Returning

--- magnum/src/main/scala/com/augustnagro/magnum/Returning.scala
+++ magnum/src/main/scala/com/augustnagro/magnum/Returning.scala
@@ -21,3 +21,24 @@ case class Returning[E](frag: Frag, reader: DbCodec[E]):
     ) match
       case Success(res) => res
       case Failure(t)   => throw SqlException(frag, t)
+
+  /** Streaming [[Iterator]]. Set [[fetchSize]] to give the JDBC driver a hint
+    * as to how many rows to fetch per request
+    */
+  def iterator(
+      fetchSize: Int = 0
+  )(using con: DbCon, use: Manager): Iterator[E] =
+    logSql(frag)
+    try
+      val ps = use(con.connection.prepareStatement(frag.sqlString))
+      ps.setFetchSize(fetchSize)
+      frag.writer.write(ps, 1)
+      val hasResults = ps.execute()
+      if hasResults then
+        val rs = use(ps.getResultSet())
+        ResultSetIterator(rs, frag, reader)
+      else
+        throw UnsupportedOperationException("No results for RETURNING clause")
+    catch case t => throw SqlException(frag, t)
+
+end Returning
