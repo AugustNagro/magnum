cc3f861bf7ac00cf669469a381009fdc7b41ef3d
lvitaly <lvitalii@gmail.com>
2025-01-23 21:06:58 +0200

    add .stripMargin method on Frag to improve multi-line formatting

--- magnum/src/main/scala/com/augustnagro/magnum/Frag.scala
+++ magnum/src/main/scala/com/augustnagro/magnum/Frag.scala
@@ -42,4 +42,29 @@ class Frag(
   ): Returning[E] =
     Returning(this, reader, colNames.columnNames.map(_.queryRepr))
 
+  /** Strips leading whitespace characters followed by a specified char from the
+    * beginning of each line in this {@link Frag} .
+    *
+    * This method is useful when you want to format SQL strings in a more
+    * readable multi-line way within your code.
+    *
+    * @param marginChar
+    *   the character that indicates the margin.
+    * @return
+    *   a new {@link Frag} instance with the modified `sqlString`.
+    */
+  def stripMargin(marginChar: Char): Frag =
+    Frag(sqlString.stripMargin(marginChar), params, writer)
+
+  /** Strips leading whitespace characters followed by a vertical bar (`|`) from
+    * the beginning of each line in this {@link Frag} .
+    *
+    * This method is useful when you want to format SQL strings in a more
+    * readable multi-line way within your code.
+    *
+    * @return
+    *   a new {@link Frag} instance with the modified `sqlString`.
+    */
+  def stripMargin: Frag = stripMargin('|')
+
 end Frag
