d9d247d733d08ef6225e1bdb36b688c18464601f
augustnagro <augustnagro@gmail.com>
2025-01-23 00:30:09 -0800

    fix macro

--- magnum-pg/src/main/scala/com/augustnagro/magnum/pg/enums/PgEnumDbCodec.scala
+++ magnum-pg/src/main/scala/com/augustnagro/magnum/pg/enums/PgEnumDbCodec.scala
@@ -2,7 +2,7 @@ package com.augustnagro.magnum.pg.enums
 
 import com.augustnagro.magnum.{DbCodec, DerivingUtil}
 
-import java.sql.{JDBCType, PreparedStatement, ResultSet}
+import java.sql.{JDBCType, PreparedStatement, ResultSet, Types}
 import scala.deriving.Mirror
 import scala.compiletime.{
   constValue,
@@ -16,12 +16,9 @@ import scala.quoted.*
 import scala.reflect.ClassTag
 
 inline given PgEnumDbCodec[A <: scala.reflect.Enum: Mirror.SumOf]: DbCodec[A] =
-  val codec = DbCodec.derived[A]
-  pgEnumDbCodecImpl[A]
+  ${ pgEnumDbCodecImpl[A] }
 
-private def pgEnumDbCodecImpl[A: Type](baseCodec: Expr[DbCodec[A]])(using
-    Quotes
-): Expr[DbCodec[A]] =
+private def pgEnumDbCodecImpl[A: Type](using Quotes): Expr[DbCodec[A]] =
   import quotes.reflect.*
   val mirror = Expr.summon[Mirror.SumOf[A]].getOrElse {
     report.errorAndAbort(
@@ -36,6 +33,7 @@ private def pgEnumDbCodecImpl[A: Type](baseCodec: Expr[DbCodec[A]])(using
             type MirroredLabel = mel
           }
         } =>
+      val nameMapExpr = DerivingUtil.buildSqlNameMapForEnum[A, mels, mets]
       val melExpr = Expr(Type.valueOfConstant[mel].get.toString)
       val sqlTypeNameExpr: Expr[String] =
         DerivingUtil
@@ -50,16 +48,35 @@ private def pgEnumDbCodecImpl[A: Type](baseCodec: Expr[DbCodec[A]])(using
           )
           .getOrElse(melExpr)
       '{
-        val c = $baseCodec
-        new DbCodec[A]:
-          def queryRepr: String = "?::" + ${ sqlTypeNameExpr }
-          def cols: IArray[Int] = c.cols
-          def readSingle(resultSet: ResultSet, pos: Int): A =
-            c.readSingle(resultSet, pos)
-          def readSingleOption(resultSet: ResultSet, pos: Int): Option[A] =
-            c.readSingleOption(resultSet, pos)
+        new DbCodec[A] {
+          val nameMap: Seq[(String, A)] = $nameMapExpr
+          val cols: IArray[Int] = IArray(Types.VARCHAR)
+          def readSingle(rs: ResultSet, pos: Int): A =
+            val str = rs.getString(pos)
+            nameMap.find((name, _) => name == str) match
+              case Some((_, v)) => v
+              case None =>
+                throw IllegalArgumentException(
+                  str + " not convertible to " + $melExpr
+                )
+          def readSingleOption(rs: ResultSet, pos: Int): Option[A] =
+            Option(rs.getString(pos)).map(str =>
+              nameMap.find((name, _) => name == str) match
+                case Some((_, v)) => v
+                case None =>
+                  throw IllegalArgumentException(
+                    str + " not convertible to " + $melExpr
+                  )
+            )
           def writeSingle(entity: A, ps: PreparedStatement, pos: Int): Unit =
-            c.writeSingle(entity, ps, pos)
+            nameMap.find((_, v) => v == entity) match
+              case Some((k, _)) => ps.setString(pos, k)
+              case None =>
+                throw IllegalArgumentException(
+                  entity.toString + " not convertible to " + $melExpr
+                )
+          def queryRepr: String = "?::" + ${ sqlTypeNameExpr }
+        }
       }
   end match
 end pgEnumDbCodecImpl
