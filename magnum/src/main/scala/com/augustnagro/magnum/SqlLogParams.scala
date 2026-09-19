4b83408d50bc6c382a9f0fa5979124dc20b2167a
augustnagro <augustnagro@gmail.com>
2026-09-19 14:48:49 -0700

    remove comment

--- magnum/src/main/scala/com/augustnagro/magnum/SqlLogParams.scala
+++ magnum/src/main/scala/com/augustnagro/magnum/SqlLogParams.scala
@@ -1,18 +1,14 @@
 package com.augustnagro.magnum
 
-/** The parameter shape used by SQL logging. */
 private[magnum] enum SqlLogParams:
   case Fragment(params: Seq[Any])
   case Single(param: Any)
   case Batch(params: Iterable[?])
 
-  /** Returns a fresh iterator so an event can be rendered more than once. */
   def iterator: Iterator[Iterator[Any]] =
     this match
       case Fragment(params) =>
         params match
-          // A single Product is one interpolated parameter whose fields are
-          // bound separately; multiple fragment parameters form one row.
           case Seq(param: Product) => Iterator(param.productIterator)
           case _                   => Iterator(params.iterator)
       case Single(param: Product) => Iterator(param.productIterator)
@@ -21,5 +17,3 @@ private[magnum] enum SqlLogParams:
         params.iterator.map:
           case param: Product => param.productIterator
           case param          => Iterator.single(param)
-
-end SqlLogParams
